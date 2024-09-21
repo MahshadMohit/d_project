8da21c39bc23f0030db5f44900ce91e19f809afc
Asoo <126341039+Maashooly@users.noreply.github.com>
2024-09-21 17:49:43 +0330

    blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+#
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def posts(request):
+    pass
+
+
+def single_post(request):
+    pass
